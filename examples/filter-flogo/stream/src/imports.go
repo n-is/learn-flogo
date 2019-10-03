ac25caee7656298c1d6fe1d0b346ebcd36e06cbb
n-is <073bex422.nischal@pcampus.edu.np>
2019-10-03 10:07:52 +0545

    Remove old app

--- examples/filter-flogo/stream/src/imports.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	_ "github.com/project-flogo/contrib/trigger/rest"
-	_ "github.com/project-flogo/stream/activity/filter"
-	_ "github.com/n-is/learn-flogo/stream/activity/filters"
-	_ "github.com/project-flogo/contrib/activity/log"
-	_ "github.com/project-flogo/stream"
-)
